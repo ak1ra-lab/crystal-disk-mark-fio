41f36a6fa165bdfe4023146de2a0fe021b48c31e
ak1ra <git@ak1ra.xyz>
2024-07-02 21:20:51 +0800

    split into multiple jobs

--- fio.sh
+++ fio.sh
@@ -1,12 +1,48 @@
-#!/bin/sh
+#!/bin/bash
 
-device=${1:-/dev/nvme0n1}
-test -b "${device}" || exit 1
+main() {
+    device="${1:-/dev/nvme0n1}"
+    test -b "${device}" || exit 1
 
-test -d results || mkdir -p results
+    action="${2:-basic}"
+    case "${action}" in
+    perf)
+        jobs=(
+            01-crystal-disk-mark-nvme-perf.fio
+        )
+        ;;
+    extra)
+        jobs=(
+            02-crystal-disk-mark-nvme-extra.fio
+        )
+        ;;
+    mix)
+        jobs=(
+            03-crystal-disk-mark-nvme-mix.fio
+        )
+        ;;
+    basic)
+        jobs=(
+            01-crystal-disk-mark-nvme-perf.fio
+            02-crystal-disk-mark-nvme-extra.fio
+        )
+        ;;
+    all)
+        jobs=(
+            01-crystal-disk-mark-nvme-perf.fio
+            02-crystal-disk-mark-nvme-extra.fio
+            03-crystal-disk-mark-nvme-mix.fio
+        )
+        ;;
+    *)
+        exit 0
+        ;;
+    esac
 
-job=crystal-disk-mark-nvme
-fio --filename="${device}" --output="results/${device#/dev/}-${job}-$(date +%F.%s).txt" "${job}.fio"
+    test -d results || mkdir -p results
+    for job in "${jobs[@]}"; do
+        fio --filename="${device}" --output="results/${device##*/}-${job%.fio}-$(date +%F.%s).txt" "${job}"
+    done
+}
 
-job=crystal-disk-mark-nvme-mix
-fio --filename="${device}" --output="results/${device#/dev/}-${job}-$(date +%F.%s).txt" "${job}.fio"
+main "$@"
